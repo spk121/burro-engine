6e37457fc50fd7b0aea4ef2409876f4c6d29ec01
Mike Gran <spk121@yahoo.com>
2013-01-04 07:51:02 -0800

    Change socket to timer

--- gui/socket.c
+++ gui/socket.c
@@ -5,7 +5,7 @@
 
 #define SOCKET_PORT (50002)
 
-GSocketClient *m_socket_client;
+GSocket *m_socket;
 
 static gboolean io_watch_cb(GIOChannel *channel, GIOCondition cond, gpointer data)
 {
@@ -17,46 +17,57 @@ static gboolean io_watch_cb(GIOChannel *channel, GIOCondition cond, gpointer dat
     {
         do_commands_from_string (str, length);
         g_free (str);
-
-        return TRUE;
     }
-    return FALSE;
+    else
+        return FALSE;
+
+    return TRUE;
+}
+
+static gboolean
+io_timer_cb(gpointer data)
+{
+    GIOChannel *ch = data;
+    gchar *str;
+    gsize length;
+    xg_io_channel_read_to_end(ch, &str, &length);
+    if (length > 0)
+    {
+        do_commands_from_string (str, length);
+    }
+    g_free (str);
+
+    return TRUE;
 }
 
 void
 initialize_socket()
 {
-    GSocketConnection *socket_connection;
-    GSocket *socket;
+    GSocketConnectable *address;
+    GSocketAddress *socket_address;
     int fd;
     GIOChannel *channel;
     const char *hello = "HELLO\r\n";
-
-    m_socket_client = xg_socket_client_new();
-    socket_connection = xg_socket_client_connect_to_host(m_socket_client, "localhost", SOCKET_PORT);
-    socket = xg_socket_connection_get_socket(socket_connection);
-    fd = xg_socket_get_fd(socket);
-
-    channel = xg_io_channel_unix_new(fd);
-
-    g_io_channel_set_flags(channel, 
-                           g_io_channel_get_flags(channel) & (~G_IO_FLAG_NONBLOCK),
-                           NULL);
+    
+    m_socket = g_socket_new (G_SOCKET_FAMILY_IPV4,
+                             G_SOCKET_TYPE_STREAM,
+                             G_SOCKET_PROTOCOL_TCP,
+                             NULL);
+    address = g_network_address_new ("localhost", SOCKET_PORT);
+    socket_address = g_socket_address_enumerator_next (g_socket_connectable_enumerate (address), NULL, NULL);
+    g_socket_connect (m_socket, socket_address, NULL, NULL);
 
     /* Declare oneself to the game server */
-    xg_socket_send (socket, hello, strlen (hello));
+    xg_socket_send (m_socket, hello, strlen (hello));
 
-    /* Setup callback */
-    xg_io_add_watch(channel, G_IO_IN, io_watch_cb, NULL);
-
-    g_io_channel_set_close_on_unref (channel, TRUE);
-    g_io_channel_unref (channel);
+    g_timeout_add(10, io_timer_cb, 
+                  g_io_channel_unix_new (g_socket_get_fd (m_socket)));
 }
 
 void
 finalize_socket ()
 {
-    g_object_unref(m_socket_client);
+    g_object_unref(m_socket);
 }
 /*
   Local Variables:
