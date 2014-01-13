429eda8edeac34572ba448c946c71a968976e1e3
Mike Gran <spk121@yahoo.com>
2014-01-13 15:03:37 -0800

    Update bg.h
    more docstrings

--- src/y/bg.h
+++ src/y/bg.h
@@ -110,11 +110,9 @@ void bg_hide (int id);
     \param type
         the type of background to init: BG_TYPE_MAP, BG_TYPE_BMP8 or BG_TYPE_BMP16
     \param width
-        width of the background in pixels (for BMP8 or BMP16), or 8x8 blocks (for MAP)
+        width of the background in pixels. For BG_TYPE_MAP, must be divisible by 8.
     \param height
-        height of the background in pixels or blocks
-    \return
-        none
+        height of the background in pixels. For BG_TYPE_MAP, must be divisible by 8.
 */
 void bg_init (int id, bg_type_t type, guint width, guint height);
 
@@ -126,21 +124,134 @@ void bg_init (int id, bg_type_t type, guint width, guint height);
 */ 
 void bg_rotate (int id, double angle);
 
+/*! \brief Scrolls the background by the specified relative values.
+         Specifically, it is a relative move of the rotational center of the
+         background.
+    \param id
+        background layer ID, e.g. BG_MAIN_0
+    \param dx
+        the horizontal scroll
+    \param dy
+        vertical scroll
+    \note
+        Be mindful of the ordering of the rotation, expansion, and scroll operations
+*/ 
 void bg_scroll (int id, double dx, double dy);
+
+/*! \brief Sets the rotation and scale of the background in one operation.
+    \param id
+        background layer ID, e.g. BG_MAIN_0
+    \param rotation
+        the angle of counter clockwise rotation about the rotational center in degrees
+    \param expansion
+        the expansion about the rotational center of the background, where 1.0 is
+        no expansion
+    \param scroll_x
+        the offset of the rotational center of the background from the screen origin
+    \param scroll_y
+        the offset of the rotational center of the background from the screen origin
+    \param rotation_center_x
+        the pixel location of the (unrotated, unexpanded) background that will be its
+        rotational center
+    \param rotation_center_y
+        the pixel location of the (unrotated, unexpanded) background that will be its
+        rotational center
+*/ 
 void bg_set (int id, double rotation, double expansion, double scroll_x, double scroll_y,
 	     double rotation_center_x, double rotation_center_y);
+
+/*! \brief Sets the center of rotation of a background
+    \param id
+        background layer ID, e.g. BG_MAIN_0
+    \param rotation_center_x
+        the pixel location of the (unrotated, unexpanded) background that will be its
+        rotational center
+    \param rotation_center_y
+        the pixel location of the (unrotated, unexpanded) background that will be its
+        rotational center
+*/
 void bg_set_rotation_center (int id, double rotation_center_x, double rotation_center_y);
+
+/*! \brief Sets the background priority
+    \param id
+        background layer id returned from bgInit or bgInitSub
+    \param priority
+        background priority (0-3) where 0 is most in the background and 3 is most 
+        in the foreground
+*/ 
 void bg_set_priority (int id, int priority);
+
+/*! \brief Performs sets the rotation of the background to specified angle. 
+        This rotation is about its rotational center.
+    \param id
+        background layer ID
+    \param rotation
+        the angle of counter clockwise rotation in degrees
+*/ 
 void bg_set_rotation (int id, double rotation);
+
+/*! \brief Performs sets the rotation and expansion of a background.
+        This rotation and expansion is about its rotational center.
+    \param id
+        background layer ID
+    \param rotation
+        the angle of counter clockwise rotation in degrees
+    \param expansion
+        the expansion ratio, where 1.0 is 1:1 expansion
+*/ 
 void bg_set_rotation_expansion (int id, double rotation, double expansion);
+
+/*! \brief Performs sets the expansion of a background.
+        This expansion is about its rotational center.
+    \param id
+        background layer ID
+    \param expansion
+        the expansion ratio, where 1.0 is 1:1 expansion
+*/ 
 void bg_set_expansion (int id, double expansion);
+
+/*! \brief Shows (makes visible) the specified background layer
+    \param id
+        background layer ID. e.g. BG_MAIN_0
+*/ 
 void bg_show (int id);
-void bg_set_map_from_tga (int id, targa_image_t *t);
-void bg_set_tilesheet_from_tga (int id, targa_image_t *t);
-void bg_set_bmp8_from_tga (int id, targa_image_t *t);
-void bg_set_bmp8_from_resource (int id, const gchar *resource);
-void bg_set_bmp16_from_tga (int id, targa_image_t *t);
-void bg_set_bmp16_from_resource (int id, const gchar *resource);
+
+/*! \brief Sets the map part of a BG_TYPE_MAP map-and-tile background from a resource.
+        Each pixel in the map image will be interpreted as the index to an 8x8 pixel
+        block in the tile image, where each block in the tile image is numbered
+        row-wise sequentially.
+    \param id
+        background layer ID, e.g. BG_MAIN_0
+    \param
+        name of image resource in the GResource bundle
+*/
+void bg_set_map_from_resource (int id, const char *resource);
+
+/*! \brief Sets the tilesheet of a BG_TYPE_MAP map-and-tile background from a resource.
+        The tilesheet is interpreted as 8x8 pixel blocks, numbered sequentially rowwise.
+    \param id
+        background layer ID, e.g. BG_MAIN_0
+    \param
+        name of image resource in the GResource bundle
+*/
+void bg_set_tilesheet_from_resource (int id, const char *resource);
+
+/*! \brief Sets the a BG_TYPE_BMP8 background from a resource.
+    \param id
+        background layer ID, e.g. BG_MAIN_0
+    \param
+        name of image resource in the GResource bundle
+*/
+void bg_set_bmp8_from_resource (int id, const char *resource);
+
+
+/*! \brief Sets the a BG_TYPE_BMP16 background from a resource.
+    \param id
+        background layer ID, e.g. BG_MAIN_0
+    \param
+        name of image resource in the GResource bundle
+*/
+void bg_set_bmp16_from_resource (int id, const char *resource);
 void bg_get_transform (int id, double *scroll_x, double *scroll_y, double *rotation_center_x,
 		       double *rotation_center_y, double *rotation, double *expansion);
 		       
