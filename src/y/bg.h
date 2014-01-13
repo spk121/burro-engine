48c826be4064266938b60e47b6b0997f851726db
Mike Gran <spk121@yahoo.com>
2014-01-13 14:05:06 -0800

    Update bg.h
    Add some docstrings.

--- src/y/bg.h
+++ src/y/bg.h
@@ -1,3 +1,30 @@
+/*---------------------------------------------------------------------------------
+
+	background.h -- definitions for DS backgrounds
+	Copyright (C) 2013,2014
+		Michael L. Gran (spk121)
+
+	GPL3+
+---------------------------------------------------------------------------------*/
+/*   |    1    |    2    |    3    |    4    |    5    |    6    |    7    |    8
+/*! \file bg.h
+    \brief background defines and functionality
+<div class="fileHeader">
+    These are the public as well as the internal API to control backgrounds.
+    There are four backgrounds for both the main and sub screens.  BG0 is 
+    the one most in the foreground and BG3 is the one most in the background.
+
+    Each background can be one of 3 different forms:
+    - a 16-bit ARGB1555 bitmap
+    - an 8-bit indexed bitmap with a 15-bit RGB555 colormap where color #0 is
+      always transparent.
+    - an 8-bit map that points to 8 by 8 pixel tiles in a separate 8-bit indexed
+      bitmap with a 15-bit RGB555 colormap
+
+</div>
+      
+*/
+
 #ifndef BURRO_BG_H
 #define BURRO_BG_H
 
@@ -39,11 +66,12 @@ enum bg_index_tag
 
 typedef enum bg_index_tag bg_index_t;
 
+//! Allowed background types, used in bg_init
 enum bg_type_tag
   {
-    BG_TYPE_MAP,
-    BG_TYPE_BMP8,
-    BG_TYPE_BMP16,
+    BG_TYPE_MAP, //!< 8bpp Tiled background with 16 bit tile indexes and no allowed rotation or scaling
+    BG_TYPE_BMP8, //!< Bitmap background with 8 bit color values which index into a 256-color RGB555 palette, where color #0 is transparent
+    BG_TYPE_BMP16, //!< Bitmap background with 16 bit color values of the form ABGR1555
   };
 
 typedef enum bg_type_tag bg_type_t;
@@ -55,11 +83,49 @@ guint16 *bg_get_map_ptr (int id);
 guint8 *bg_get_tilesheet_ptr (int id);
 guint8 *bg_get_bmp8_ptr (int id);
 guint16 *bg_get_bmp16_ptr (int id);
+
+/*! \brief Gets the priority of the background layer.
+    \param id
+        background layer id. e.g. BG_MAIN_0
+    \return
+        priority 0, 1, 2, or 3
+*/
 int bg_get_priority (int id);
+
 gboolean bg_is_shown (int id);
+
+/*! \brief Hides the current background
+    \param id
+        background layer ID. e.g. BG_MAIN_0
+*/ 
 void bg_hide (int id);
+
+/*! \brief Initializes a background on either the main or sub displays
+           Sets up the format and size of a background layer.  Also resets its
+           rotation and scaling back to 1:1 and 0 degress of rotation.  Any
+           bitmap resource associated with the background will be reset.
+           The priority will be reset to 0, 1, 2 or 3 respectively.
+    \param id
+        background layer to init. Must be BG_MAIN_0 , 1, 2, 3 or BG_SUB_0, 1, 2, 3
+    \param type
+        the type of background to init: BG_TYPE_MAP, BG_TYPE_BMP8 or BG_TYPE_BMP16
+    \param width
+        width of the background in pixels (for BMP8 or BMP16), or 8x8 blocks (for MAP)
+    \param height
+        height of the background in pixels or blocks
+    \return
+        none
+*/
 void bg_init (int id, bg_type_t type, guint width, guint height);
+
+/*! \brief Performs a cumulative rotation of the background by the specified angle. 
+    \param id
+        background layer ID
+    \param angle
+        the angle of counter clockwise rotation in degrees
+*/ 
 void bg_rotate (int id, double angle);
+
 void bg_scroll (int id, double dx, double dy);
 void bg_set (int id, double rotation, double expansion, double scroll_x, double scroll_y,
 	     double rotation_center_x, double rotation_center_y);
