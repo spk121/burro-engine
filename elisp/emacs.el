1939654fbae69b648ebee2c9d81be18f597d9811
Mike Gran <spk121@yahoo.com>
2014-09-12 06:16:15 -0700

    No tabs in emacs startup

--- elisp/emacs.el
+++ elisp/emacs.el
@@ -2,7 +2,7 @@
 
 (require 'cc-mode)
 (setq-default c-basic-offset 4 c-default-style "linux")
-(setq-default tab-width 4 indent-tabs-mode t)
+(setq-default tab-width 4 indent-tabs-mode nil)
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
 
 (require 'autopair)
