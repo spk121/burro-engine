6413ce38eb3365d4b8777ab7b2b29ab04fbf0583
Mike Gran <spk121@yahoo.com>
2014-09-11 22:52:21 -0700

    Add a helper header to hide spidermonkey warnings

---
+++ src/jsapi.hpp
@@ -0,0 +1,8 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weffc++"
+#pragma GCC diagnostic ignored "-Winvalid-offsetof"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
+#include <jsapi.h>
+#pragma GCC diagnostic pop
