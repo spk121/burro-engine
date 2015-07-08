7c3c9506a169672b5f52758b37d7abc84fe95170
Mike Gran <mgran@localhost.localdomain>
2015-07-08 15:45:55 -0700

    Make x-wrapped functions a static library and pch header

---
+++ engine/x/x.h
@@ -0,0 +1,11 @@
+#ifndef BURRO_LIBX_H
+#define BURRO_LIBX_H
+#include <xcairo.h>
+#include <xgdk.h>
+#include <xgdk-pixbuf.h>
+#include <xgio.h>
+#include <xglib.h>
+#include <xgtk.h>
+#include <xguile.h>
+#include <xpulseaudio.h>
+#endif
