5e1d5b19a3de0a91228080f7f40f1ea25b0c7f85
Mike Gran <spk121@yahoo.com>
2015-07-24 03:12:54 -0700

    Add repl experiment junk

---
+++ engine/y/repl.c
@@ -0,0 +1,32 @@
+//  This is the connection to the guile REPL
+#include "../x.h"
+
+SCM repl_server_socket = SCM_UNSPECIFIED;
+SCM repl_server_object = SCM_UNSPECIFIED;
+
+void
+repl_init ()
+{
+    scm_c_eval_string ("(use-modules (system repl server) (system repl coop-server))");
+    repl_server_socket = scm_c_eval_string("(make-tcp-server-socket #:port 37147)");
+    repl_server_object = scm_call_1 (scm_c_eval_string ("spawn-coop-repl-server"),
+                                         repl_server_socket);
+}
+
+void
+repl_tick ()
+{
+    static int first = 1;
+    static SCM func = SCM_BOOL_F;;
+    if (repl_server_object != SCM_UNSPECIFIED)
+    {
+        if (first == 1)
+        {
+            func = scm_c_eval_string ("poll-coop-repl-server");
+            first = 0;
+        }
+        scm_call_1 (func, repl_server_object);
+    }
+}
+
+
