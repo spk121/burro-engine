25d5945ada962ef4f62c8791f86b3f4cf1350bf0
Mike Gran <spk121@yahoo.com>
2015-08-20 16:31:21 -0700

    Some background images and scripts

--- engine/tmx.scm
+++ engine/tmx.scm
@@ -1,21 +1,27 @@
 (use-modules (json))
 
 (define fp (open-file "brickroom1.json" "r"))
-(define js (json->scm fp))
+(define x (json->scm fp))
 
-;; These have to be less than or equal to the map size
-;; for each layer used
-(define map-height (hash-ref js "height"))
-(define map-width (hash-ref js "width"))
+;; This is the overall size of the map, in tiles
+(define map-height (hash-ref x "height"))
+(define map-width (hash-ref x "width"))
 
+;; This is the size of a tile.
 ;; These have to match Burro's TILE_HEIGHT and TILE_WIDTH
 (hash-ref x "tileheight")
 (hash-ref x "tilewidth")
+
+;; This needs to be the string "right-down"
+(hash-ref x "renderorder")
 
 ;; Our maps can only have 1 tilesheet, so this has to be 0
 ;; if the backgrounds are all BMP or exactly 1
 ;; if there are any maps
 (length (hash-ref x "tilesets"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define TS (car (hash-ref x "tilesets")))
 
 ;; These need to match Burro's TILE_HEIGHT and TILE_WIDTH
@@ -26,8 +32,9 @@
 (hash-ref TS "firstgid")
 (hash-ref TS "spacing")
 
-;; This needs to be the string "right-down"
-(hash-ref x "renderorder")
+;; This is the sizeof the tilesheet, in pixels
+(hash-ref TS "imageheight")
+(hash-ref TS "imagewidth")
 
 ;; This is the stride, in tiles, of the tilesheet.
 ;; We need this to convert tile indices in maps to
@@ -36,6 +43,9 @@
 
 ;; This is the filename of the tilesheet
 (hash-ref TS "image")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; This is a list of all the layers
 (hash-ref x "layers")
@@ -57,12 +67,33 @@
 
 ;; Say "L" is a particular map layer
 
-;; These need to fit within the matrix
+;; This is the size of a layer in tiles
 (hash-ref L "height")
 (hash-ref L "width")
 
 ;; This sets if a layer is initially "shown" or "hidden"
 (hash-ref L "visible")
+
+;; This is the global opacity of a layer.  This is probably
+;; going to be ignored.
+(hash-ref L "opacity")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; BMP LAYERS
+
+;; This is the type of a BMP layer
+(string=? "imagelayer" (hash-ref L "type"))
+
+;; This is the filename of the image
+(hash-ref L "image")
+
+;; Note that the actual size of the image has nothing to
+;; do with the size of the layer in tiles.  If the image
+;; is too big, it can be truncated.  If it is too small,
+;; it can be padded with transparency.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MAP LAYERS
 
 ;; This is the actual map, a 1D list of indices
 ;; These indices need to be converted in Burro indices for
@@ -76,14 +107,38 @@
 (define bv (bg-get-bytevector BG_MAIN_0))
 (bytevector-u32-set! bv (* 4 map-location-index) tilesheet-index-and-flags)
 ;; the '4' is for sizeof(uint32_t)
-;; the map-location-index is the location of an entry on the BG_XXX_X map
-;; layer.
+
+;; This is where it gets a bit tricky.
+;; For TMX, the map has a given height and width in tiles,
+;; and the tilesheet has a given height and width in tiles.
+
+;; For Burro, the map and the tilesheet has to be one of the
+;; pre-approved MATRIX_ sizes
+
+;; burroMapRow = dataIndex / tmxMapWidth
+;; burroMapCol = dataIndex % tmxMapWidth
+;; burroMapIndex = burroMapRow * burroMapWidth + burroMapCol
+
+;; The indices inside of the map itself also need to be adjusted
+;; burroTilesheetRow = dataVal / tmxTilesheetWidth
+;; burroTilesheetCol = dataVal / tmxTilesheetWidth
+;; burroTilesheetIndex = burroTilesheetRow * burroTilesheetWidth
+;;                       + burroTilesheetCol
+
+;; Keep in mind that a layer's MATRIX_ size may be larger or
+;; smaller than the TMX file's, so the map may need to be
+;; truncated or zero-padded.
+
 ;; The tilesheet-index-and-flags is the 32-bit location of a tile in the
 ;; associated tilesheet, along with the associated vert, horiz, and diag
 ;; flip flags
 ;; The update the whole rendering of the bg layer with an explicit call
 ;; 'bg-update'
 (bg-update BG_MAIN_0)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; OBJECT LAYERS
 
 ;; Burro only allows one "objectgroup" layer per TMX map
 
