6f0c8487ed01e7472c88b6a185a51c5858036a27
Mike Gran <spk121@yahoo.com>
2018-03-20 08:26:17 -0700

    Better bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
-# This will run autoconf, automake, etc
-autoreconf --force --install
+# Run this to generate all the initial makefiles, etc.
+# MLG - I cribbed this script from GNOME's default autogen.sh
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd $srcdir
+
+(test -f configure.ac) || {
+        echo "*** ERROR: Directory "\`$srcdir\'" does not look like the top-level project directory ***"
+        exit 1
+}
+
+aclocal --install || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+
+cd $olddir
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure "$@" || exit 1
+        echo "Now type 'make' to compile."
+else
+        echo "Skipping configure process."
+fi
